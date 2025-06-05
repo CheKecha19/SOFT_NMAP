b1eb401860d437d2c9cba944a6529ceea621ef03
cybertsigan <61182912+CheKecha19@users.noreply.github.com>
2025-06-05 13:59:51 +0300

    Create config.py

---
+++ script/config.py
@@ -0,0 +1,13 @@
+# config.py
+# Цвета для состояний портов в формате HEX
+COLORS = {
+    'open': '90EE90',       # Зеленый
+    'closed': 'FFCCCB',     # Красный
+    'filtered': 'FFFFE0',   # Желтый
+    'undefined': 'D3D3D3',  # Светло-серый
+    'default': 'FFFFFF'     # Белый
+}
+
+# Настройки по умолчанию
+DEFAULT_OUTPUT_DIR = r"{PATH_PATH_PATH}" 
+OUTPUT_SUFFIX = '_filtered.xlsx'
